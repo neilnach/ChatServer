32c417bb490ca680a74096d45592c106d13b1f1f
Neil Nachnani <nnachnan@purdue.edu>
2017-01-25 16:53:34 -0500

    Update User.java

--- User.java
+++ User.java
@@ -1,15 +1,4 @@
 /**
- * <b> CS 180 - Project 4 - Chat Server Skeleton </b>
- * <p>
- * <p>
- * This is the skeleton code for the ChatServer Class. This is a private chat
- * server for you and your friends to communicate.
- *
- * @author Neil Nachnani nnachnan@purdue.edu
- *         Tejaswi Kotekar tkotekar@purdue.edu
- * @version November 20th, 2015
- * @lab Neil - BR8
- * Tejaswi - LC2
  */
 public class User {
     String userName;
